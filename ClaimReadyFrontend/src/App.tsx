f7a16ba13294d1157ae24989233a7d8613085b51
adiparekh944 <adityaparekh944@gmail.com>
2025-03-10 18:50:54 -0500

    changes to old frontend.
    removed navbar and the other pages, just kept the homepage and functionality as it was during the MadData25 hackathon presentaiton

--- ClaimReadyFrontend/src/App.tsx
+++ ClaimReadyFrontend/src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Navbar from './Navbar'; 
 import Homepage from './Homepage';
-import UploadPage from './UploadPage';
+
 export default function App() {
   return (
     <Router>
       <div className="p-4 bg-background">
-        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage/>} />
-          <Route path="/UploadPage" element={<UploadPage/>} />
         </Routes>
       </div>
     </Router>
